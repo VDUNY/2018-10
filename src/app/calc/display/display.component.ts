feb0cde994ab2bc216c0e4cc8ccc6c04203f6185
Joe Pietruch <percipient24@gmail.com>
2018-10-24 23:39:05 -0400

    display using service

--- src/app/calc/display/display.component.ts
+++ src/app/calc/display/display.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DisplayService } from './display.service';
 
 @Component({
   selector: 'app-display',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DisplayComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private display: DisplayService
+  ) { }
 
-  ngOnInit() {
-  }
+  ngOnInit() { }
 
 }
