4cd0b17e2a8c3f7d6084425a04c4b76ee01ab87e
Joe Pietruch <percipient24@gmail.com>
2018-10-25 01:42:12 -0400

    e2e tests

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,4 +1,5 @@
 import { AppPage } from './app.po';
+import { browser, protractor } from 'protractor';
 
 describe('workspace-project App', () => {
   let page: AppPage;
@@ -7,8 +8,118 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('should navigate to the main page', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to demo-calc!');
+  });
+
+  it('should smash the 1 button 10 times', () => {
+    page.click(page.b1);
+    page.click(page.b1);
+    page.click(page.b1);
+    page.click(page.b1);
+    page.click(page.b1);
+    page.click(page.b1);
+    page.click(page.b1);
+    page.click(page.b1);
+    page.click(page.b1);
+    page.click(page.b1);
+
+    page.display.getText().then((displayText: String) => {
+      expect(displayText).toEqual('1111111111');
+    });
+  });
+
+  it('should clear the text', () => {
+    page.click(page.bClear);
+
+    page.display.getText().then((displayText: String) => {
+      expect(displayText).toEqual('0');
+    });
+  });
+
+  it('should flip the calculator', () => {
+    page.calc.isPresent().then((isPresent: boolean) => {
+      expect(isPresent).toBeTruthy();
+    });
+    page.invertedCalc.isPresent().then((isPresent: boolean) => {
+      expect(isPresent).toBeFalsy();
+    });
+
+    page.click(page.bFlip);
+    browser.sleep(page.animationTimeout);
+
+    page.calc.isPresent().then((isPresent: boolean) => {
+      expect(isPresent).toBeFalsy();
+    });
+    page.invertedCalc.isPresent().then((isPresent: boolean) => {
+      expect(isPresent).toBeTruthy();
+    });
+  });
+
+  it('should flip the calculator back', () => {
+    page.click(page.bFlip);
+    browser.sleep(page.animationTimeout);
+
+    page.calc.isPresent().then((isPresent: boolean) => {
+      expect(isPresent).toBeTruthy();
+    });
+    page.invertedCalc.isPresent().then((isPresent: boolean) => {
+      expect(isPresent).toBeFalsy();
+    });
+  });
+
+  it('should smash the other buttons', () => {
+    page.click(page.b2);
+    page.click(page.b3);
+    page.click(page.b4);
+    page.click(page.bDot);
+    page.click(page.b5);
+    page.click(page.b6);
+    page.click(page.b7);
+    page.click(page.b8);
+    page.click(page.b9);
+    page.click(page.b0);
+
+    page.display.getText().then((displayText: String) => {
+      expect(displayText).toEqual('234.567890');
+    });
+  });
+
+  it('should clear with keyboard', () => {
+    browser.actions().sendKeys('C').perform();
+
+    page.display.getText().then((displayText: String) => {
+      expect(displayText).toEqual('0');
+    });
+  });
+
+  it('should type with keyboard', () => {
+    browser.actions().sendKeys('1234567890').perform();
+
+    page.display.getText().then((displayText: String) => {
+      expect(displayText).toEqual('1234567890');
+    });
+  });
+
+  it('should do a trick', () => {
+    browser.actions().sendKeys('C').perform();
+    browser.actions().sendKeys('.07734').perform();
+    page.click(page.footer);
+    browser.actions().sendKeys(protractor.Key.SPACE).perform();
+    browser.sleep(page.animationTimeout);
+
+    page.calc.isPresent().then((isPresent: boolean) => {
+      expect(isPresent).toBeFalsy();
+    });
+    page.invertedCalc.isPresent().then((isPresent: boolean) => {
+      expect(isPresent).toBeTruthy();
+    });
+    page.display.getText().then((displayText: String) => {
+      expect(displayText).toEqual('0.07734');
+    });
+  });
+
+  it('should pause for admiration', () => {
+    browser.sleep(page.defaultTimeout);
   });
 });
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -1,11 +1,42 @@
-import { browser, by, element } from 'protractor';
+import { browser, by, element, ElementFinder, promise, ProtractorExpectedConditions, protractor } from 'protractor';
 
 export class AppPage {
+  defaultTimeout: number = 120000;
+  animationTimeout: number = 1000;
+  EC: ProtractorExpectedConditions = protractor.ExpectedConditions;
+
+  footer: ElementFinder = element(by.css('footer'));
+
+  b1: ElementFinder = element(by.cssContainingText('app-button button', '1'));
+  b2: ElementFinder = element(by.cssContainingText('app-button button', '2'));
+  b3: ElementFinder = element(by.cssContainingText('app-button button', '3'));
+  b4: ElementFinder = element(by.cssContainingText('app-button button', '4'));
+  b5: ElementFinder = element(by.cssContainingText('app-button button', '5'));
+  b6: ElementFinder = element(by.cssContainingText('app-button button', '6'));
+  b7: ElementFinder = element(by.cssContainingText('app-button button', '7'));
+  b8: ElementFinder = element(by.cssContainingText('app-button button', '8'));
+  b9: ElementFinder = element(by.cssContainingText('app-button button', '9'));
+  b0: ElementFinder = element(by.cssContainingText('app-button button', '0'));
+  bDot: ElementFinder = element(by.cssContainingText('app-button button', '.'));
+  bClear: ElementFinder = element(by.cssContainingText('app-button button', 'C'));
+  bFlip: ElementFinder = element(by.cssContainingText('app-button button', 'flip'));
+
+  display: ElementFinder = element(by.css('app-display .numbers'));
+
+  calc: ElementFinder = element(by.css('app-calc .calculator:not(.inverted)'));
+  invertedCalc: ElementFinder = element(by.css('app-calc .calculator.inverted'));
+
   navigateTo() {
     return browser.get('/');
   }
 
   getParagraphText() {
     return element(by.css('app-root h1')).getText();
+  }
+
+  click(elem: ElementFinder): promise.Promise<void> {
+    browser.wait(this.EC.elementToBeClickable(elem), this.defaultTimeout);
+
+    return browser.actions().mouseMove(elem).click().perform();
   }
 }
