9e7cb1bcf548ff71fcd8b448b25cac4d75839641
Joe Pietruch <percipient24@gmail.com>
2018-10-25 00:05:27 -0400

    keyboard input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, HostListener } from '@angular/core';
+import { CalcComponent } from './calc/calc.component';
+import { DisplayService } from './calc/display/display.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  @ViewChild(CalcComponent)
+  calc: CalcComponent;
+
+  private keys: string[] = [
+    'Digit1',
+    'Digit2',
+    'Digit3',
+    'Digit4',
+    'Digit5',
+    'Digit6',
+    'Digit7',
+    'Digit8',
+    'Digit9',
+    'Digit0',
+    'KeyC',
+    'Period',
+    'Space',
+  ];
+
+  constructor(
+    private display: DisplayService
+  ) {}
+
+  @HostListener('document:keydown', ['$event'])
+  handleKeyDown(event: KeyboardEvent): void {
+    if (this.keys.indexOf(event.code) === -1) {
+      return;
+    }
+
+    console.log(event);
+
+    if (event.code === 'KeyC') {
+      this.display.clearDisplay();
+    }
+    else if (event.code === 'Space' && this.calc) {
+      this.calc.flip();
+    }
+    else {
+      this.display.addToDisplay(event.key);
+    }
+  }
 }
